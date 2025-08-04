a3af17243405e6353627586c606c63d877822c0f
khobaess <m2208632@edu.misis.ru>
2025-08-04 17:19:19 +0300

    5

--- src/main/java/com/example/resumedev/Config/CorsConfig.java
+++ src/main/java/com/example/resumedev/Config/CorsConfig.java
@@ -1,34 +1,17 @@
 package com.example.resumedev.Config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowedOriginPatterns(Arrays.asList(
-                "https://stage-app53924188-*.pages.vk-apps.com",
-                "https://vk.com",
-                "https://mobile.vk.com"
-        ));
-
-        config.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
-        config.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/**", config);
-        return source;
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(CorsConfiguration.ALL)
+                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowedHeaders("*");
     }
 }
